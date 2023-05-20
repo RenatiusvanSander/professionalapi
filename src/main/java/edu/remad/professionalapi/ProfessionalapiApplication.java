5934d0cbee3fc213b2edfdb2b3a6433c4761ff62
RenatiusvanSander / R. Meier <remad@web.de>
2023-05-20 18:57:10 +0200

    master: initial commit

---
+++ src/main/java/edu/remad/professionalapi/ProfessionalapiApplication.java
@@ -0,0 +1,13 @@
+package edu.remad.professionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/remad/professionalapi/ProfessionalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.remad.professionalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
