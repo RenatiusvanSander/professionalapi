91a20151175c912aa0a736cf9ff765b815906dec
RenatiusvanSander / R. Meier <remad@web.de>
2023-06-11 01:00:20 +0200

    chapter4: refactoring migration sql, because syntax errors

--- src/main/resources/db/migration/V1__Initial_version.sql
+++ src/main/resources/db/migration/V1__Initial_version.sql
@@ -1,18 +1,31 @@
 create schema if not exists ecomm;
--- Other script tags
-create TABLE IF NOT EXISTS ecomm.cart (
-  id uuid NOT NULL,
-user_id uuid NOT NULL,
-FOREIGN KEY (user_id)
-  REFERENCES ecomm.user(id),
-PRIMARY KEY(id)
+create TABLE IF NOT EXISTS ecomm.user (
+	id uuid NOT NULL DEFAULT random_uuid(),
+	username varchar(16),
+	password varchar(40),
+	first_name varchar(16),
+	last_name varchar(16),
+	email varchar(24),
+	phone varchar(24),
+	user_status varchar(16)
 );
-create TABLE IF NOT EXISTS ecomm.cart_item (
-cart_id uuid NOT NULL,
-item_id uuid NOT NULL,
-FOREIGN KEY (cart_id)
-  REFERENCES ecomm.cart(id),
-FOREIGN KEY(item_id)
-  REFERENCES ecomm.item(id)
+create TABLE IF NOT EXISTS ecomm.product (
+	id uuid NOT NULL DEFAULT random_uuid() PRIMARY KEY,
+	name varchar(56) NOT NULL,
+	description varchar(200),
+	price numeric(16, 4) DEFAULT 0 NOT NULL,
+	count numeric(8, 0),
+	image_url varchar(40)
 );
--- other SQL scripts
+CREATE TABLE IF NOT EXISTS ecomm.cart(
+id uuid NOT NULL PRIMARY KEY,
+user_id uuid NOT NULL
+);
+create TABLE IF NOT EXISTS ecomm.item (
+	id uuid NOT NULL DEFAULT random_uuid(),
+	product_id uuid NOT NULL,
+	quantity numeric(8, 0),
+	unit_price numeric(16, 4) NOT NULL,
+	PRIMARY KEY(id)
+);
+create TABLE IF NOT EXISTS ecomm.cart_item (cart_id uuid NOT NULL, item_id uuid NOT NULL, FOREIGN KEY (cart_id) REFERENCES ecomm.cart(id));
